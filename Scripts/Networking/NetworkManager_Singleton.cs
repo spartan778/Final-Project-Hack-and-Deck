9fa633a934d05b9ba0915ea0583b8d35ead7c63a
Brian-Laptop2 <heatproject117@hotmail.com>
2026-06-14 20:16:11 +0100

    Moved Networking Manager to a global script

---
+++ Scripts/Networking/NetworkManager_Singleton.cs
@@ -0,0 +1,290 @@
+using Godot;
+using System;
+using System.Collections;
+using HCoroutines;
+using Godot.Collections; 
+using Array = Godot.Collections.Array;
+
+public partial class NetworkManager_Singleton : Node
+{
+	public static NetworkManager_Singleton NetworkManagerSingleton;
+	private WebRtcPeerConnection peerConnection;
+	private WebRtcMultiplayerPeer rtcMultiplayerPeer;
+	private WebSocketPeer webSocket = new WebSocketPeer();
+	private int addCount = 0;
+	private bool isHost;
+	public bool IsHost => isHost;
+	public Action SignalServerConnected, PlayerMatched, RtcConnected;
+	public Action<int> PlayerCountChanged;
+
+	[Export] private string localAddress = "ws://localhost:8080";
+	[Export] private string onlineAddress = "wss://final-project-hack-and-deck.onrender.com/";
+	[Export] private bool testLocal;
+	private bool offerHandled;
+	[Export] private string roomName = "PlayRoom";
+	private int deviceId;
+	private int peerId;
+
+	private Json jsonHelper;
+
+	public override void _Ready()
+	{
+		NetworkManagerSingleton = this; //set a global reference
+		ConnectSignals();
+		PrepareConnection();
+	}
+	
+	private void ConnectSignals()
+	{
+		Multiplayer.PeerConnected += (id) => // inform player about connection status
+		{
+			GD.Print($"Peer connected ID: {id}");
+			if (isHost)
+			{
+				
+				GD.Print($"Playing as host (ID: {deviceId})");
+			}
+			else
+			{
+				GD.Print($"Playing as client (ID: {deviceId})"); ;
+			}
+			RtcConnected?.Invoke();
+		};
+		Multiplayer.PeerDisconnected += (id) => // Debugging with peer id
+		{
+			GD.Print($"Peer disconnected ID: {id}");
+		};
+	}
+
+	private void PrepareConnection()
+	{
+		jsonHelper = new Json();
+		var config = new Dictionary
+		{
+			{ "iceServers", new Array
+				{
+					new Dictionary { { "urls", "stun:stun.l.google.com:19302" } }, // fallback free google stun service
+					new Dictionary {
+						{ "urls", "turn:global.relay.metered.ca:80" }, //registered (free) TURN service from Metered
+						{ "username", "1268560a6c410cc2b0dee1f3" },
+						{ "credential", "CF7DHme8j4F+IYpn" }
+					},
+					new Dictionary { 
+						{ "urls", "turn:global.relay.metered.ca:443" },
+						{ "username", "1268560a6c410cc2b0dee1f3" },
+						{ "credential", "CF7DHme8j4F+IYpn" }
+					},
+				}
+			}
+		};
+		peerConnection = new WebRtcPeerConnection();
+		peerConnection.Initialize(config);
+		peerConnection.SessionDescriptionCreated += (type, sdp) => //SDP process
+		{
+			GD.Print("SessionDescriptionCreated");
+			peerConnection.SetLocalDescription(type, sdp);
+			SendSignal(new Dictionary
+			{
+				{ "room", roomName },
+				{ "type", type },
+				{ "sdp", sdp }
+			});
+		};
+
+		peerConnection.IceCandidateCreated += (media, index, name) => //ICE process
+		{
+			GD.Print("IceCandidateCreated");
+			GD.Print($"ICE: {name}");
+			SendSignal(new Dictionary
+			{
+				{ "room", roomName },
+
+				{ "type", "ice" },
+				{ "media", media },
+				{ "index", index },
+				{ "name", name }
+			});
+		};
+		
+	}
+
+	public override void _Process(double delta)
+	{
+		webSocket.Poll();
+		peerConnection.Poll();
+		if (webSocket.GetReadyState() == WebSocketPeer.State.Open)
+		{
+			while (webSocket.GetAvailablePacketCount() > 0)
+			{
+				var msg = webSocket.GetPacket().GetStringFromUtf8();
+				HandleSignal(msg);
+			}
+		}
+	}
+	
+	private void SendSignal(Dictionary data)
+	{
+		webSocket.SendText(Json.Stringify(data));
+	}
+
+	private void HandleSignal(string msg)
+	{
+		var err = jsonHelper.Parse(msg);
+		if (err != Error.Ok)
+		{
+			GD.PrintErr("JSON parse error: ", jsonHelper.GetErrorMessage());
+			return;
+		}
+		var data = Json.ParseString(msg).AsGodotDictionary(); //convert JSON to Dictionary
+		
+		if(!data.TryGetValue("room", out var roomValue)) return; // Block all message with a "Room"
+		if(roomValue.ToString() !=  roomName) return;
+		var type = data["type"].ToString();
+		GD.Print($"HandlingSignal type: {type}");
+		switch (type)
+		{
+			case "playerCountUpdate":
+			{
+				var playerCount = (int)data["count"];
+				GD.Print($"PlayerCount: {playerCount}");
+				PlayerCountChanged.Invoke(playerCount);
+				break;
+			}
+			case "matchReady":
+			{
+				if (isHost)
+				{
+					GD.Print($"Player Matched, Press Make Offer to start game");
+				}
+				else
+				{
+					GD.Print($"Player Matched, wait for host to start game");
+				}
+				PlayerMatched?.Invoke();
+				break;
+			}
+			// handle first stage
+			case "offer" or "answer":
+			{
+				peerConnection.SetRemoteDescription(type, data["sdp"].ToString());
+				GD.Print($"RTC msg type: {type}");
+				break;
+			}
+			case "ice":
+			{
+				peerConnection.AddIceCandidate( // media, index , name
+					data["media"].ToString(),
+					(int)data["index"],
+					data["name"].ToString()
+				);
+				break;
+			}
+		}
+	}
+	
+	private string GetServerAddress() //return connection address depending on if testing locally
+	{
+		return testLocal ? localAddress : onlineAddress;
+	}
+
+	public async void StartSignalingConnection()
+	{
+		 
+		webSocket.ConnectToUrl(GetServerAddress());
+		// GD.Print("webSocket connecting");
+		// GD.Print("Is Host: " + isHost);
+		
+		
+		while (webSocket.GetReadyState() != WebSocketPeer.State.Open) //wait until Web Socket is Open
+		{
+			await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		}
+		SendJoinMsg();
+		if (isHost) //switch ID to match on two peers
+		{
+			deviceId = 1;
+			peerId = 2;
+		}
+		else
+		{
+			deviceId = 2;
+			peerId = 1;
+		}
+		
+		rtcMultiplayerPeer = new WebRtcMultiplayerPeer();
+		rtcMultiplayerPeer.CreateMesh(deviceId);
+		rtcMultiplayerPeer.AddPeer(peerConnection,peerId);
+		Multiplayer.MultiplayerPeer = rtcMultiplayerPeer; //attach peer to Godot high level API (RPC)
+		GD.Print("Signal Server connected");
+		SignalServerConnected?.Invoke();
+		Co.Run(OnDurationRepeat(2f));
+	}
+
+	public void StartRtcProcess()
+	{
+		peerConnection.CreateOffer();
+	}
+	
+	public void SetJoinAsHost(bool value)
+	{
+		isHost = value;
+		if (isHost)
+		{
+			GD.Print("Joining as Host");
+		}
+		else
+		{
+			GD.Print("Joining as Client");
+		}
+	}
+
+	private IEnumerator OnDurationRepeat(float duration) //mostly used for debugging
+	{
+		while (true)
+		{
+			float timer = 0;
+			while (timer<duration)
+			{
+				timer += (float)GetProcessDeltaTime();
+				yield return null;
+			}
+
+			var connectionStatus= rtcMultiplayerPeer.GetConnectionStatus();
+			var peerAmount = rtcMultiplayerPeer.GetPeers().Count;
+			
+			GD.Print($"RTC Connection: {connectionStatus}");
+			GD.Print($"RTC Peer Count: {peerAmount}");
+			if (connectionStatus == MultiplayerPeer.ConnectionStatus.Connected)
+			{
+				Rpc(nameof(TestRpc_Add));
+			}
+		}
+	}
+	
+	private void SendJoinMsg() //send joining message to make sure both player are in the room
+	{
+		SendSignal(new Dictionary
+		{
+			{ "room", roomName },
+			{ "type", "join" }
+		});
+	}
+
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer)]
+	private void TestRpc_Add()
+	{
+		addCount++;
+		GD.Print($"RTC running: addCount: {addCount}");
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("menu_confirm"))
+		{
+			var connectionStatus = rtcMultiplayerPeer.GetConnectionStatus();
+			if(connectionStatus != MultiplayerPeer.ConnectionStatus.Connected) return;
+			Rpc(nameof(TestRpc_Add));
+		}
+	}
+	
+}
---
+++ SharedScenes/ConnectionMenu.cs
@@ -0,0 +1,87 @@
+using Godot;
+using System;
+
+public partial class ConnectionMenu : Control
+{
+    private NetworkManager_Singleton networkManagerSingleton;
+    [Export] private CheckButton isHostButton;
+    [Export] private Button connectButton, makeOfferButton;
+    [Export] private Label infoLabel;
+
+    public override void _EnterTree()
+    {
+        networkManagerSingleton = GetNode<NetworkManager_Singleton>("/root/NetworkManagerSingleton");
+        GD.Print($"Node Ref: {networkManagerSingleton.Name}");
+    }
+
+    public override void _Ready()
+    {
+        makeOfferButton.Disabled = true;
+        isHostButton.SetPressedNoSignal(false);
+        ConnectSignals();
+    }
+
+    private void ConnectSignals()
+    {
+        isHostButton.Pressed += OnIsHostButtonPressed;
+        connectButton.Pressed += OnConnectButtonPressed;
+        makeOfferButton.Pressed += OnMakeOfferButtonPressed;
+        networkManagerSingleton.SignalServerConnected += OnSignalServerConnected;
+        networkManagerSingleton.PlayerMatched += OnPlayerMatched;
+        networkManagerSingleton.PlayerCountChanged += OnPlayerCountChanged;
+        networkManagerSingleton.RtcConnected += OnRtcConnected;
+    }
+
+    private void OnMakeOfferButtonPressed()
+    {
+        networkManagerSingleton.StartRtcProcess();
+    }
+
+    private void OnConnectButtonPressed()
+    {
+        networkManagerSingleton.StartSignalingConnection();
+        connectButton.Disabled = true;
+        infoLabel.Text = "Connecting to server...";
+    }
+
+    private void OnIsHostButtonPressed()
+    {
+        var state= isHostButton.ButtonPressed;
+        var text = state? "Host" : "Client";
+        infoLabel.Text = $"Joining as: {text}";
+        networkManagerSingleton.SetJoinAsHost(state);
+    }
+    
+    private void OnSignalServerConnected()
+    {
+        connectButton.Disabled = true;
+        infoLabel.Text = "Signal Server Connected," +
+                         "\n waiting for the other player";
+    }
+
+    private void OnPlayerMatched()
+    {
+        if (networkManagerSingleton.IsHost)
+        {
+            infoLabel.Text = "Player Matched, Click [Make Offer] to create P2P connection as Host";
+            makeOfferButton.Disabled = false;
+        }
+        else
+        {
+            infoLabel.Text = "Player Matched, wait for Host to start P2P connection";
+        }
+    }
+
+    private void OnRtcConnected()
+    {
+        var state= isHostButton.ButtonPressed;
+        var text = state? "Host" : "Client";
+        infoLabel.Text = $"P2P connection established, you are playing as: {text}";
+        makeOfferButton.Disabled = true;
+    }
+
+    private void OnPlayerCountChanged(int playerCount)
+    {
+        GD.Print($"Current Player Count: {playerCount}");
+    }
+}
