f1bb858b4e57b57480fffc999b44f7c78548ef04
spartan778 <heatproject117@hotmail.com>
2026-08-17 19:34:50 +0800

    (Android) Completed: Drag input with touch

--- Scripts/CardGame/CardSlots/MotionDetectionArea.cs
+++ Scripts/CardGame/CardSlots/MotionDetectionArea.cs
@@ -12,6 +12,7 @@ public partial class MotionDetectionArea : Area2D
     private bool isHovered, isPressed = false;
     private Coroutine currentSwipeCoroutine;
     private Vector2 startPosition, endPosition;
+    private int touchIndex;
     public override void _Ready()
     {
         ConnectSignals();
@@ -19,8 +20,8 @@ public partial class MotionDetectionArea : Area2D
 
     private void ConnectSignals()
     {
-        MouseEntered += OnMouseEntered;
-        MouseExited += OnMouseExited;
+        // MouseEntered += OnMouseEntered; // disabling raw mouse input for testing
+        // MouseExited += OnMouseExited;
     }
     private void OnMouseEntered()
     {
@@ -32,7 +33,7 @@ public partial class MotionDetectionArea : Area2D
     }
     
     
-    public override void _Input(InputEvent @event)
+    public override void _Input(InputEvent @event) // legacy method used by mouse input
     {
         if(PokerGameManagerRef.IsDragging) return; // only detect if not dragging poker
         if(CardSlots.IsInCoolDown) return; // only detect if cardSlots are not in cooldown
@@ -51,8 +52,64 @@ public partial class MotionDetectionArea : Area2D
             }
         }
     }
+
+    public override void _UnhandledInput(InputEvent @event) // later learnt that _Input should only be used for UI level input
+    {
+        if(PokerGameManagerRef.IsDragging) return; // only detect if not dragging poker
+        if(CardSlots.IsInCoolDown) return; // only detect if cardSlots are not in cooldown
+        if (@event is InputEventScreenTouch screenTouch)
+        {
+            if ( screenTouch.Pressed && IsPosInsideArea(screenTouch.Position) )
+            {
+                startPosition = screenTouch.Position;
+                GD.Print($"Starting (touch) swipe at position {startPosition}");
+                isPressed = true;
+                touchIndex = screenTouch.Index;
+                startPosition = screenTouch.Position;
+            }
+            if (screenTouch.Index == touchIndex && !screenTouch.Pressed && isPressed)
+            {
+                GD.Print("Swipe cancelled (released)");
+                swipeProgress.Value = 0;
+                isPressed = false;
+            }
+        }
+        if(!isPressed) return; // no need to track touch motion if the area is not pressed
+        if (@event is InputEventScreenDrag screenDrag)
+        {
+            var currentTouchPosition = screenDrag.Position;
+            var distance = currentTouchPosition - startPosition;
+            swipeProgress.Value = (distance.X / swipeDistance)*100;
+            if (distance.X > swipeDistance)
+            {
+                GD.Print("Swipe successful(Touch)");
+                CardSlots.SwipeSuccess?.Invoke();
+            }
+        }
+    }
     
-    private IEnumerator TrackSwipeCoroutine()
+    private bool IsPosInsideArea(Vector2 pos) // raycast to detect if touch hits the interaction area
+    {
+        var spaceState = GetWorld2D().DirectSpaceState; // get reference to the physical state
+        var query = new PhysicsPointQueryParameters2D // set raycast to detect Area2D ONLY
+        {
+            Position = pos,
+            CollideWithAreas = true,
+            CollideWithBodies = false
+        };
+        var results = spaceState.IntersectPoint(query); // the actual raycast query
+
+        foreach (var result in results)
+        {
+            if (result["collider"].As<Node>() == this) // find node from collider
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator TrackSwipeCoroutine() // legacy method used by mouse input
     {
         while (isPressed)
         {
